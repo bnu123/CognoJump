739e20541b009a2433c147324816ab6f1fd7f08a
bnu123 <kash.bhanu7@gmail.com>
2019-06-17 18:17:33 +0530

    Speed calculation initialised

--- src/components/Pages/PageElements/Indicators.js
+++ src/components/Pages/PageElements/Indicators.js
@@ -2,25 +2,22 @@ import React from 'react'
 import '../../../css/indicator.css'
 
 const Indicators = (
-                {speed_value = 10,
-                error_value = 20 ,
-                error_frequency_key = 'Q',
-                }
+               props 
             ) => {
     return (
         <div className="container">
            <div className="row">
                 <div className="col-2 text-center">
                     <span className="indicator-text">Speed :                </span>
-                    <span className="indicator-value">{speed_value}</span>
+                    <span className="indicator-value">{props.speed_value}</span>
                 </div>
                 <div className="col-2">
                     <span className="indicator-text">Errors : </span>
-                    <span className="indicator-value">{error_value}</span>
+                    {/* <span className="indicator-value">{error_value}</span> */}
                 </div>
                 <div className="col-2">
                     <span className="indicator-text">Gain : </span>
-                    <span className="indicator-value">{error_frequency_key}</span>
+                    {/* <span className="indicator-value">{error_frequency_key}</span> */}
                 </div>
                 <div className="col-4 text-right" >
                    <button className="btn btn-primary"type="button">Advance Settings</button> 
--- src/components/Pages/SpeedPage.js
+++ src/components/Pages/SpeedPage.js
@@ -1,50 +1,51 @@
+//react imports 
 import React, { Component } from 'react';
+
+//components import 
 import Keyboard from './PageElements/Keyboard';
 import TypeBox from './PageElements/TypeBox';
-
-import '../../css/SpeedPage.css';
-
 import {special_characters, changed_characters, colors} from '../../utility';
 import Indicators from './PageElements/Indicators';
 
- const get_key = (tes) => {    
-        //check if special character
-        var parsed_key = parseInt(tes);
-        var key = tes;
+import '../../css/SpeedPage.css';
 
-        if (tes in special_characters){
-            key = special_characters[tes];
-            return key;
-        }
+const get_key = (tes) => {    
+    //check if special character
+    var parsed_key = parseInt(tes);
+    var key = tes;
 
-        if (tes.length === 1){
-            //check for digit
-            if(!isNaN(parsed_key)){
-                key = `Digit${tes}`;
-            }
-            //Normal key pressed
-            else{
-                //capitalise key e.g. a--> 
-                var capital = tes.toUpperCase();
-                key = `Key${capital}`;
-            }
-        }
-
-        else {
-            if (key in changed_characters){
-                key = changed_characters[key];
-            }
-
-            else
-                key = tes;
-        }
-
+    if (tes in special_characters){
+        key = special_characters[tes];
         return key;
     }
 
+    if (tes.length === 1){
+        //check for digit
+        if(!isNaN(parsed_key)){
+            key = `Digit${tes}`;
+        }
+        //Normal key pressed
+        else{
+            //capitalise key e.g. a--> 
+            var capital = tes.toUpperCase();
+            key = `Key${capital}`;
+        }
+    }
+
+    else {
+        if (key in changed_characters){
+            key = changed_characters[key];
+        }
+
+        else
+            key = tes;
+    }
+
+    return key;
+}
 
 class SpeedPage extends Component {
-
+    time = 0;
     state = {
        text : this.get_text(),
        span_text : "",
@@ -52,33 +53,32 @@ class SpeedPage extends Component {
        //define color for each keykk
        color : colors,
        textLength : this.get_text().length,
+       initialLength : this.get_text().length,
+       speed : 0,
+       start_time : 0,
+       end_time : 0,
 
     }
 
+
     get_text(char){
-        // text from server
-       return "He, who had seen the glory in death, curse in life and loneliness around people shall achieve greatness in life";
+        // text from server //api callback 
+       return "He, who have seen the glory in death, chaos in life and misery in rich shall become god";
     }
 
 
     onkeyDown = (e) => {
-        /*
-        Initial Thoughts : 
-                convert the  key into say
-                when 'e' is pressed, it should become KeyE
-                when Ctrl is pressed, it should become .
-                
-        Then Found Out : https://javascript.info/keyboard-events#summary
-                There is 'code' which displays the key position, so now no
-                need to convert it. 
-    
-        Then Again found out : e.code does not work, it gives undefined as return
-        */
         e.preventDefault();
-        var raw = e.key;
-        var k = get_key(raw);
-
-        if (k in this.state.color){
+        let raw = e.key;
+        let k = get_key(raw);
+        let current_text = this.state.text;
+        let { textLength, initialLength, color} = this.state;
+        //first key is clicked
+        if(textLength === initialLength){
+            this.setState((state)=>({...state,start_time : new Date()})); 
+        }
+        //Keyboard key color change handler
+        if (k in color){
             this.setState(prevState => (
                 {
                     color : {
@@ -88,44 +88,51 @@ class SpeedPage extends Component {
                 }
             ))
         }
-
-        var x = this.state.text;
-
-        if(this.state.textLength === 1 && x.charAt(0) === raw){
-            this.setState((prevState)=>({
-                        ...prevState,
-                        text : this.get_text(),
-                        span_text : "",
-                        textLength : this.get_text().length,
-            }
-            ));
-            return ; 
-        }
-
-        
-        if(x.charAt(0) === raw){
-            var str = x.charAt(0);
+        // General cases : change text color only
+        if(current_text.charAt(0) === raw){
+            var str = current_text.charAt(0);
             var span_text = this.state.span_text;
             span_text = span_text + str;
-            let length = this.state.textLength - 1;
+            let l = textLength - 1;
 
             this.setState((prevState)=>({
                 ...prevState,
-                text : x.substring(1),
+                text : current_text.substring(1),
                 span_text : span_text,
-                textLength : length, 
-
+                textLength : l, 
             }));
+
             return ;
         }
+        /*
+            Last character handler
+            if at last character : do following:
+                                    ~ calculate values(speed, errors,etc ).
+                                    ~ get new text.
+        */
+        if(textLength === 1 && current_text.charAt(0) === raw){
+            this.setState((prevState)=>({...prevState,end_time : new Date()}), 
+                ()=>{
+                    let time_elapsed = ((this.state.end_time - this.state.start_time)/1000)/60; //minutest
+                    let speed = ((this.state.initialLength/5)/time_elapsed);
 
-       
+                    this.setState((prevState)=>(
+                        {...prevState,
+                        speed : speed,
+                        text : this.get_text(),
+                        span_text : "",
+                        textLength : this.get_text().length,}));
+                });
+
+            return ; 
+        }
     }
+
 
     onkeyUp = (e) => {
         var raw = e.key;
         var k = get_key(raw);
-
+        //just change key color 
         if (k in this.state.color){
             this.setState(prevState => (
                 {
@@ -136,24 +143,20 @@ class SpeedPage extends Component {
                 }
             ))
         }
-
     }
     
   render() {
 
     return (
-    
-    <div>
+    <div className="some-class active">
         <header className="Header">
         </header>
         <div  onKeyDown={this.onkeyDown} onKeyUp={this.onkeyUp} tabIndex="0" >
-            <Indicators />
+            <Indicators speed_value={this.state.speed}/>
             <TypeBox main_text={this.state.text} span_text={this.state.span_text} />
             <Keyboard color={this.state.color}/>
-            
         </div>
     </div>
-   
     );
   }
 }
