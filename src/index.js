3bdd4ab27ba15ef98423b2ea90ab0a19315d976e
bnu123 <kash.bhanu7@gmail.com>
2019-06-18 02:28:40 +0530

    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import rootReducer from './rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk)),
+);
 
 
-
-import { BrowserRouter } from 'react-router-dom';
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+        <BrowserRouter>
+        <Provider store={store}>
+        <App />
+        </Provider>
+        </BrowserRouter>, 
+        document.getElementById('root'));
